3b1b799f4e5a596680668f3cb7195d010cccf868
jvkusto <vladimir.kusto@jvion.com>
2018-05-14 15:36:29 -0400

    Added Queue class.

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const UtilsHelper = require('./utils-helper.js'),
-	Logger = require('./console-logger.js');
+	Logger = require('./console-logger.js'),
+	Queue = require('./queue.js');
 
 var Utils = function(){
 	this.UtilsHelper = UtilsHelper;
 	this.Logger = Logger;
+	this.Queue = Queue;
 }
 
 module.exports = new Utils();
